ed900fcdfae0ee9bc27f0c6304c1e804fa438d7b
tomilho1 <tomfrancisco1111@gmail.com>
2023-08-01 22:35:21 -0300

    add readTrackPointer function

--- hex-utils.js
+++ hex-utils.js
@@ -22,7 +22,26 @@ function readLE(buffer) {
     return sum
 }
 
+/**
+ * Pointers stored inside tracks are 2 bytes only (1 bank range). It is possible, though, that a track,
+ * like the Battle Theme, starts at bank C8 and ends at bank C9, meaning that they occupy 2 banks instead.
+ * That means that just reading the pointers in the track is not enough, we need to further verify
+ * if the pointer is actually pointing to an address that is **in the range of the track**. Note that
+ * this correction is only rarely neccessary (in a vanilla ROM, it is only needed in the Battle Theme). Also,
+ * tracks can't be larger than 0xFFFF bytes anyway.
+ */
+function readTrackPointer(pointer, trackAddress, lastByteAddress) {
+    pointer = readLE(pointer) + (trackAddress & 0xFF0000)
+    if (pointer < trackAddress) {
+        pointer = pointer + 0x010000
+    } else if (pointer > lastByteAddress) {
+        pointer = pointer - 0x010000
+    }
+    return pointer
+}
+
 module.exports = {
     toHex,
-    readLE
+    readLE,
+    readTrackPointer
 }
